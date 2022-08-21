178e43c21d0b88c93e66de567bbb692d9a22c524
Tobias Mieves <tobias.mieves@tu-dortmund.de>
2022-08-21 11:25:11 +0200

    feat: Add quantity to wishes
    It is now possible to indicate that you need more than one of the items.
    These wishes can be reserved by multiple users.
    
    Signed-off-by: Tobias Mieves <tobias.mieves@tu-dortmund.de>

--- app/models.py
+++ app/models.py
@@ -1,18 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
 
 
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
 class Wish(models.Model):
-	text = models.TextField(max_length=1000)
-	link = models.URLField(verbose_name="Link", null=True)
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	wish_for = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wish_of")
-	reserved_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reserved_wish", null=True)
+    text = models.TextField(max_length=1000)
+    link = models.URLField(verbose_name="Link", null=True)
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    wish_for = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wish_of")
+    reserved_by = models.ManyToManyField(User, related_name="reserved_wishes")
 
 
 class Group(models.Model):
-	name = models.TextField(max_length=100)
-	users = models.ManyToManyField(User, related_name='wish_groups')
+    name = models.TextField(max_length=100)
+    users = models.ManyToManyField(User, related_name='wish_groups')
 
-	def __str__(self):
-		return "<Group {}>".format(self.name)
+    def __str__(self):
+        return "<Group {}>".format(self.name)
--- app/views.py
+++ app/views.py
@@ -9,79 +9,83 @@ from app.models import Wish, Group
 
 @login_required
 def homeView(request):
-	return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.user.id}))
+    return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.user.id}))
 
 
 @login_required
 def wishListView(request, list_owner):
-	# Get all user which are in one of the users groups
-	print(Group.objects.count())
-	if Group.objects.count() > 0:
-		groups = request.user.wish_groups.all()
-		userSet = set()  # set only allows unique values
-		for group in groups:
-			userSet.update(list(group.users.all()))
-		users = list(userSet)
-	else:
-		users = User.objects.all()
-	# Get all requests except those that are from others for the user
-	list_owner = get_object_or_404(User, pk=list_owner)
-	if list_owner == request.user:
-		wishes = Wish.objects.filter(wish_for=list_owner).filter(owner=request.user)
-	else:
-		wishes = Wish.objects.filter(wish_for=list_owner)
+    # Get all user which are in one of the users groups
+    print(Group.objects.count())
+    if Group.objects.count() > 0:
+        groups = request.user.wish_groups.all()
+        userSet = set()  # set only allows unique values
+        for group in groups:
+            userSet.update(list(group.users.all()))
+        users = list(userSet)
+    else:
+        users = User.objects.all()
+    # Get all requests except those that are from others for the user
+    list_owner = get_object_or_404(User, pk=list_owner)
+    if list_owner == request.user:
+        wishes = Wish.objects.filter(wish_for=list_owner).filter(owner=request.user)
+    else:
+        wishes = Wish.objects.filter(wish_for=list_owner)
 
-	return render(request, "app/own-wish-list.html",
-	              context={'wishes': wishes, 'list_owner': list_owner, 'all_users': users})
+    return render(request, "app/own-wish-list.html",
+                  context={'wishes': wishes, 'list_owner': list_owner, 'all_users': users})
 
 
 @login_required
 def newWishView(request):
-	text = request.POST['wish-text']
-	link = request.POST['wish-link']
-	list_owner = get_object_or_404(User, pk=request.POST['list_owner'])
-	newWish = Wish(text=text, link=link, owner=request.user, wish_for=list_owner)
-	if request.user != list_owner:
-		newWish.reserved_by = request.user
-	newWish.save()
-	return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.POST['list_owner']}))
+    text = request.POST['wish-text']
+    link = request.POST['wish-link']
+    quantity = request.POST['wish-quantity']
+    list_owner = get_object_or_404(User, pk=request.POST['list_owner'])
+    newWish = Wish(text=text, link=link, quantity=quantity, owner=request.user, wish_for=list_owner)
+    newWish.save()
+    if request.user != list_owner:
+        newWish.reserved_by.add(request.user)
+        newWish.save()
+    return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.POST['list_owner']}))
 
 
 @login_required
 def deleteWishView(request, wish_id):
-	wish = get_object_or_404(Wish, pk=wish_id)
-	if wish.owner == request.user:
-		wish.delete()
-	return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.GET['list_owner']}))
+    wish = get_object_or_404(Wish, pk=wish_id)
+    if wish.owner == request.user:
+        wish.delete()
+    return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.GET['list_owner']}))
 
 
 @login_required
 def reserveWishView(request, wish_id):
-	wish = get_object_or_404(Wish, pk=wish_id)
-	if wish.wish_for != request.user and not wish.reserved_by:
-		wish.reserved_by = request.user
-		wish.save()
-	return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.GET['list_owner']}))
+    wish = get_object_or_404(Wish, pk=wish_id)
+    if wish.wish_for != request.user and len(wish.reserved_by.all()) < wish.quantity:
+        wish.reserved_by.add(request.user)
+        wish.save()
+    return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.GET['list_owner']}))
 
 
 @login_required
 def cancelReserveWishView(request, wish_id):
-	wish = get_object_or_404(Wish, pk=wish_id)
-	if wish.reserved_by == request.user:
-		wish.reserved_by = None
-		wish.save()
-	return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.GET['list_owner']}))
+    wish = get_object_or_404(Wish, pk=wish_id)
+    if wish.reserved_by.contains(request.user):
+        wish.reserved_by.remove(request.user)
+        wish.save()
+    return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.GET['list_owner']}))
 
 
 @login_required
 def editWishView(request, wish_id):
-	wish = get_object_or_404(Wish, pk=wish_id)
-	if request.method == 'POST':
-		text = request.POST['wish-text']
-		link = request.POST['wish-link']
-		wish.text = text
-		wish.link = link
-		wish.save()
-		return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.POST['list_owner']}))
-	else:
-		return render(request, 'app/edit-wish.html', {'list_owner': request.GET['list_owner'], 'wish': wish})
+    wish = get_object_or_404(Wish, pk=wish_id)
+    if request.method == 'POST':
+        text = request.POST['wish-text']
+        link = request.POST['wish-link']
+        quantity = request.POST['wish-quantity']
+        wish.text = text
+        wish.link = link
+        wish.quantity = quantity
+        wish.save()
+        return HttpResponseRedirect(reverse('wishList', kwargs={'list_owner': request.POST['list_owner']}))
+    else:
+        return render(request, 'app/edit-wish.html', {'list_owner': request.GET['list_owner'], 'wish': wish})
