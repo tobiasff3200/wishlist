96ae14cc7a8b964c9fa07a1ac682ee9bad356ae4
Tobias Mieves <tobias.mieves@tu-dortmund.de>
2025-05-04 22:31:35 +0200

    feat: Add social account login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-	// add daisyUI plugin
-	plugins: [require("daisyui")],
 	content: ["./templates/**/*.{html,js,ts}", "wishlist/templates/**/*.{html,js,ts}"],
-
-	// daisyUI config (optional)
-	daisyui: {
-		themes: ['light', 'dark']
-	}
 };
